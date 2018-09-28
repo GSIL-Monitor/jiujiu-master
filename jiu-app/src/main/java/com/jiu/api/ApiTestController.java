75cc67706718b126f8f4ffa250a4fb3e9d7f7243
lukai <lukai@hanxinbank.com>
2018-09-28 15:53:53 +0800

    add

--- jiu-app/src/main/java/com/jiu/api/ApiTestController.java
+++ jiu-app/src/main/java/com/jiu/api/ApiTestController.java
@@ -2,16 +2,18 @@ package com.jiu.api;
 
 import com.jiu.client.TestFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by lukai on 2018/8/2.
  */
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequestMapping
 public class ApiTestController {
 
     @Autowired
@@ -21,12 +23,19 @@ public class ApiTestController {
     RestTemplate restTemplate;
 
     @RequestMapping(value = "/getMessage",method = {RequestMethod.GET,RequestMethod.POST})
+    @ResponseBody
     public String getMessage(){
         return testClient.getMessage();
     }
 
     @RequestMapping(value = "/getMessage2",method = {RequestMethod.GET,RequestMethod.POST})
+    @ResponseBody
     public String getMessage2(){
         return restTemplate.postForObject("http://JIU-SERVICE-A:9100/test/getMessage",null,String.class);
+    }
+
+    @RequestMapping(value = "/toMessage",method = {RequestMethod.GET,RequestMethod.POST})
+    public String toMessage(){
+        return "test";
     }
 }
--- jiu-service-a/src/test/java/com/jiu/JiuServiceAApplicationTests.java
+++ jiu-service-a/src/test/java/com/jiu/JiuServiceAApplicationTests.java
@@ -5,12 +5,32 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JiuServiceAApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void test(){
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("1","19908623115");
+		map.put("2","18696111395");
+		List<String> strlist = new ArrayList<String>();
+		strlist.add("1");
+		strlist.add("3");
+		strlist.add("4");
+
+		strlist.stream().forEach((s) -> {
+			System.out.println(s);
+		});
 	}
 
 }
