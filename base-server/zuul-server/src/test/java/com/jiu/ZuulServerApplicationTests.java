02900e31b5f2b87ca57c08e185aef51d4c48c86b
lukai <hbwhlukai@163.com>
2018-10-26 21:28:19 +0800

    ##

--- base-server/zuul-server/src/test/java/com/jiu/ZuulServerApplicationTests.java
+++ base-server/zuul-server/src/test/java/com/jiu/ZuulServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ZullServerApplicationTests {
+public class ZuulServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
