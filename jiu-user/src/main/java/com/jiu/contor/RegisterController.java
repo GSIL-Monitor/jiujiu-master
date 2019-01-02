7b14e58bfec99390e0396ae5850cb94530f78a02
lukai <lukai@hanxinbank.com>
2019-01-02 09:44:32 +0800

    添加logback

--- jiu-user/src/main/java/com/jiu/contor/RegisterController.java
+++ jiu-user/src/main/java/com/jiu/contor/RegisterController.java
@@ -4,6 +4,7 @@ import com.jiu.common.annotation.LogAnnotation;
 import com.jiu.model.TUser;
 import com.jiu.service.RegisterService;
 import com.jiu.util.MailUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import java.util.List;
 /**
  * Created by lukai on 2018/10/26.
  */
+@Slf4j
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
@@ -61,5 +63,18 @@ public class RegisterController {
                 "测试", "测试内容", "lukai@hanxinbank.com",
                 "Lu2452204520");
         return succ;
+    }
+
+
+
+    @RequestMapping(value = "/log")
+    @ResponseBody
+    public String log(){
+        log.trace("trace");
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
+        return "success";
     }
 }
