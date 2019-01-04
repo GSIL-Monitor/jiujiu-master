f86c4e675abe01513788122fbaaa1277c04a5110
lukai <lukai@hanxinbank.com>
2019-01-04 17:40:57 +0800

    添加邮件

---
+++ jiu-user/src/main/java/com/jiu/service/impl/MailServiceImpl.java
@@ -0,0 +1,64 @@
+package com.jiu.service.impl;
+
+import com.jiu.service.MailService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+/**
+ * Created by lukai on 2019/1/4.
+ */
+@Slf4j
+@Service
+public class MailServiceImpl implements MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+
+    @Override
+    public void sendSimpleMail(String from, String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+
+        try {
+            mailSender.send(message);
+            log.info("简单邮件已经发送。");
+        } catch (Exception e) {
+            log.error("发送简单邮件时发生异常！", e);
+        }
+    }
+
+    @Override
+    public void sendAttachmentsMail(String from, String to, String subject, String content, String filePath) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+
+            FileSystemResource file = new FileSystemResource(new File(filePath));
+            String fileName = filePath.substring(filePath.lastIndexOf(File.separator));
+            helper.addAttachment(fileName, file);
+
+            mailSender.send(message);
+            log.info("带附件的邮件已经发送。");
+        } catch (MessagingException e) {
+            log.error("发送带附件的邮件时发生异常！", e);
+        }
+    }
+}
